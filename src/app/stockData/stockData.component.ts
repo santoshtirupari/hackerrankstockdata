5f1ad02db0e45623615e7572b28ba4ccc9b69dfc
santoshtirupari <68094162+santoshtirupari@users.noreply.github.com>
2020-07-10 00:01:54 -0400

    Updated component code

--- src/app/stockData/stockData.component.ts
+++ src/app/stockData/stockData.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {StockDataService} from "./stock-data.service";
 
 @Component({
   selector: 'stock-data',
@@ -6,8 +7,25 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./stockData.component.scss']
 })
 export class StockData implements OnInit {
-
+  public stockData;
+  public error = false;
+constructor(private readonly stockDataService: StockDataService) {
+}
   ngOnInit() {
-    
+
+  }
+  getStockData(query){
+    this.stockDataService.getStockData(query.value).subscribe((response) => {
+      this.error = false;
+      this.stockData = response;
+      console.log(this.stockData)
+      if(this.stockData && this.stockData.data.length ==0){
+        this.error = true
+      }
+    },error => {
+      {
+        this.error = true;
+      }
+    } )
   }
 }
